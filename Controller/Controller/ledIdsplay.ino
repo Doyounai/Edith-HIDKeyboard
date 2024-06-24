93f0797504010c71654ab51419bf60d53a9e3095
kong <peeradech_wongkum@cmu.ac.th>
2024-06-25 00:49:38 +0700

    update

--- Controller/Controller/ledIdsplay.ino
+++ Controller/Controller/ledIdsplay.ino
@@ -1,3 +1,5 @@
+#include <String.h>
+
 #define SCREEN_WIDTH 128  // OLED display width, in pixels
 #define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
@@ -19,18 +21,38 @@ int dateTextWidth = 0;
 int dateTextHeight = 0;
 float dateTextSize = 1.7;
 
+String read = "";
+
+int min = 0;
+int second = 0;
+
 void drawTimeText() {
-  display.setTextSize(timeTextSize);                                   // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);                                 // Draw white text
+  display.setTextSize(timeTextSize);                              // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);                            // Draw white text
   display.setCursor(timeTextX - (timeTextWidth / 2), timeTextY);  // Start at top-left corner
-  display.println(F("23:19"));
+
+  min = (millis() / 1000) / 60;
+  second = millis() / 1000;
+
+  // Create a String object with enough space for 4 characters (including null terminator)
+  String formattedString = "";
+
+  // Use sprintf to format the number with leading zero (maximum width 2)
+  sprintf(formattedString.c_str(), "%02d:%02d", min, second);
+
+  display.println(formattedString.c_str());
 }
 
 void drawDateText() {
   display.setTextSize(dateTextSize);                              // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE);                            // Draw white text
   display.setCursor(timeTextX - (dateTextWidth / 2), dateTextY);  // Start at top-left corner
-  display.println(F("24 June 2024"));
+  // display.println(F("24 June 2024"));
+  if (read != "") {
+    display.println(read);
+  } else {
+    display.println("-- -- ----");
+  }
 }
 
 void drawVolumeBar() {
@@ -59,6 +81,8 @@ void displaySetup() {
 
   display.setTextSize(dateTextSize);
   display.getTextBounds("xx xxxxxx xxxx", 0, 0, &x, &y, &dateTextWidth, &dateTextHeight);
+
+  updateDisplay();
 }
 
 void displayUpdate() {
